b9dda4819ae2d8c80c7595fe86d5ea222973482b
Chaguza <chrispin.chaguza@gmail.com>
2026-02-18 11:45:08 -0600

    Updates

--- virallc/SeqLib/SeqLib.py
+++ virallc/SeqLib/SeqLib.py
@@ -16,7 +16,7 @@ dbpath = Path.home().joinpath("viraldb")
 tmpdirpath = f"tmpR.virallc.{random.randrange(1,1000000)}.{datetime.datetime.now()}".\
                  replace(':','').replace(' ','.').replace(',','.')
 refdbName = ""
-version = "1.0.17"
+version = "1.0.18"
 
 def ReadCmdOptions():
 
